458967c99f01adf1e637297700dce2e058442038
Gil Burns <gil@gilburns.com>
2024-09-03 22:51:18 -0500

    Revert "Create Arturia Software Center label"
    This reverts commit fd73cd12f7e357eebdbae8ccf8bdb1e1d013e11e.

--- fragments/labels/arturiasoftwarecenter.sh
+++
@@ -1,9 +0,0 @@
-arturiasoftwarecenter)
-    name="Arturia Software Center"
-    type="pkg"
-    packageID="com.arturia.softwarecenter"
- 	appNewVersion=$(getJSONValue "$(curl -fsL 'https://www.arturia.com/api/resources?slugs=asc&types=soft')" '[0].version')
-	downloadVersion=$(echo "${appNewVersion}" | tr '.' '_')
-	downloadURL="https://dl.arturia.net/products/asc/soft/Arturia_Software_Center__${downloadVersion}.pkg"
-    expectedTeamID="T53ZHSF36C"
-    ;;
