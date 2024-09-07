bfb14299b719d1f5845b992613a963204c818d6c
Gil Burns <gil@gilburns.com>
2024-09-07 11:10:53 -0500

    Create u-he ACE label

---
+++ fragments/labels/uheace.sh
@@ -0,0 +1,10 @@
+uheace)
+    name="u-he Ace"
+    type="pkgInZip"
+    downloadURL=https://uhe-dl.b-cdn.net/releases/$( curl -fs "https://uhe-dl.b-cdn.net/releases/" | grep -o "ACE_[0-9.].*[0-9]_Mac.zip" | cut -d\> -f2 )
+    appNewVersion=$( echo "$downloadURL" | cut -d_ -f2 | sed 's/./&./g;s/\.$//' )
+    appCustomVersion(){
+      /usr/bin/defaults read "/Library/Application Support/u-he/ACE/Engine/ACE.engine/Contents/Info.plist" CFBundleVersion 2>/dev/null
+    }
+    expectedTeamID="5X3525NQ8W"
+    ;;
