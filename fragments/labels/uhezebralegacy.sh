85b1167fb80c8a1b051c83aa0f827c526b4012f9
Gil Burns <gil@gilburns.com>
2024-09-07 12:53:07 -0500

    Create u-he Zebra Legacy label

---
+++ fragments/labels/uhezebralegacy.sh
@@ -0,0 +1,10 @@
+uhezebralegacy)
+    name="u-he Zebra Legacy"
+    type="pkgInZip"
+    downloadURL=https://uhe-dl.b-cdn.net/releases/$( curl -fs "https://uhe-dl.b-cdn.net/releases/" | grep -o "Zebra_Legacy_[0-9.].*[0-9]_Mac.zip" | cut -d\> -f2 )
+    appNewVersion=$( echo "$downloadURL" | cut -d_ -f2 | sed 's/./&./g;s/\.$//' )
+    appCustomVersion(){
+      /usr/bin/defaults read "/Library/Audio/Plug-Ins/Components/ZebraHZ.component/Contents/Info.plist" CFBundleVersion 2>/dev/null
+    }
+    expectedTeamID="5X3525NQ8W"
+    ;;
