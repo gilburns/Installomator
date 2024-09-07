1942872f7eb902301b831d9d6421e0ddd1beba05
Gil Burns <gil@gilburns.com>
2024-09-07 11:18:24 -0500

    Create u-he Filterscape label

---
+++ fragments/labels/uhefilterscape.sh
@@ -0,0 +1,10 @@
+uhefilterscape)
+    name="u-he Flterscape"
+    type="pkgInZip"
+    downloadURL=https://uhe-dl.b-cdn.net/releases/$( curl -fs "https://uhe-dl.b-cdn.net/releases/" | grep -o "Flterscape_[0-9.].*[0-9]_Mac.zip" | cut -d\> -f2 )
+    appNewVersion=$( echo "$downloadURL" | cut -d_ -f2 | sed 's/./&./g;s/\.$//' )
+    appCustomVersion(){
+      /usr/bin/defaults read "/Library/Application Support/u-he/Flterscape/Engine/Flterscape.engine/Contents/Info.plist" CFBundleVersion 2>/dev/null
+    }
+    expectedTeamID="5X3525NQ8W"
+    ;;
