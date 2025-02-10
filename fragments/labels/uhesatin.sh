a45ede56902d7f4e634c581843ad95223da8b3ef
Gil Burns <gil@gilburns.com>
2025-02-09 21:41:24 -0600

    Update uhesatin.sh

--- fragments/labels/uhesatin.sh
+++ fragments/labels/uhesatin.sh
@@ -3,8 +3,6 @@ uhesatin)
     type="pkgInZip"
     downloadURL=https://uhe-dl.b-cdn.net/releases/$( curl -fs "https://uhe-dl.b-cdn.net/releases/" | grep -o "Satin_[0-9.].*[0-9]_Mac.zip" | cut -d\> -f2 )
     appNewVersion=$( echo "$downloadURL" | cut -d_ -f2 | sed 's/./&./g;s/\.$//' )
-    appCustomVersion(){
-      /usr/bin/defaults read "/Library/Application Support/u-he/Satin/Engine/Satin.engine/Contents/Info.plist" CFBundleVersion 2>/dev/null
-    }
+    appCustomVersion(){ /usr/bin/defaults read "/Library/Application Support/u-he/Satin/Engine/Satin.engine/Contents/Info.plist" CFBundleVersion 2>/dev/null }
     expectedTeamID="5X3525NQ8W"
     ;;
