c248824b5752bed90abee6fbf033c50e73e9200d
Gil Burns <gil@gilburns.com>
2025-02-09 21:41:48 -0600

    Update uhemfm2.sh

--- fragments/labels/uhemfm2.sh
+++ fragments/labels/uhemfm2.sh
@@ -3,8 +3,6 @@ uhemfm2)
     type="pkgInZip"
     downloadURL=https://uhe-dl.b-cdn.net/releases/$( curl -fs "https://uhe-dl.b-cdn.net/releases/" | grep -o "MFM2_[0-9.].*[0-9]_Mac.zip" | cut -d\> -f2 )
     appNewVersion=$( echo "$downloadURL" | cut -d_ -f2 | sed 's/./&./g;s/\.$//' )
-    appCustomVersion(){
-      /usr/bin/defaults read "/Library/Application Support/u-he/MFM2/Engine/MFM2.engine/Contents/Info.plist" CFBundleVersion 2>/dev/null
-    }
+    appCustomVersion(){ /usr/bin/defaults read "/Library/Application Support/u-he/MFM2/Engine/MFM2.engine/Contents/Info.plist" CFBundleVersion 2>/dev/null }
     expectedTeamID="5X3525NQ8W"
     ;;
