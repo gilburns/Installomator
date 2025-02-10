9cfc65c38ef914d98bb9af2effcf132f1330f6d2
Gil Burns <gil@gilburns.com>
2025-02-09 21:40:29 -0600

    Update uhepresswerk.sh

--- fragments/labels/uhepresswerk.sh
+++ fragments/labels/uhepresswerk.sh
@@ -3,8 +3,6 @@ uhepresswerk)
     type="pkgInZip"
     downloadURL=https://uhe-dl.b-cdn.net/releases/$( curl -fs "https://uhe-dl.b-cdn.net/releases/" | grep -o "Presswerk_[0-9.].*[0-9]_Mac.zip" | cut -d\> -f2 )
     appNewVersion=$( echo "$downloadURL" | cut -d_ -f2 | sed 's/./&./g;s/\.$//' )
-    appCustomVersion(){
-      /usr/bin/defaults read "/Library/Application Support/u-he/Presswerk/Engine/Presswerk.engine/Contents/Info.plist" CFBundleVersion 2>/dev/null
-    }
+    appCustomVersion(){ /usr/bin/defaults read "/Library/Application Support/u-he/Presswerk/Engine/Presswerk.engine/Contents/Info.plist" CFBundleVersion 2>/dev/null }
     expectedTeamID="5X3525NQ8W"
     ;;
