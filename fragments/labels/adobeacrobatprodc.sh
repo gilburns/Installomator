79077d65995df32c99039f578bfaee51a00f1625
Gil Burns <gil@gilburns.com>
2026-02-01 22:06:22 -0600

    Update adobeacrobatprodc.sh

--- fragments/labels/adobeacrobatprodc.sh
+++ fragments/labels/adobeacrobatprodc.sh
@@ -5,6 +5,7 @@ adobeacrobatprodc)
     pkgName="Acrobat/Acrobat DC Installer.pkg"
     packageID="com.adobe.acrobat.DC.viewer.app.pkg.MUI"
     downloadURL="https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/osx10/Acrobat_DC_Web_WWMUI.dmg"
+    appNewVersion=$(curl -sL https://armmf.adobe.com/arm-manifests/mac/AcrobatDC/acrobat/current_version.txt)
     expectedTeamID="JQ525L2MZD"
     blockingProcesses=( "Acrobat Pro DC" )
     Company="Adobe"
