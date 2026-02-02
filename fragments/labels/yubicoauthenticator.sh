0fae15c62d0cdf8194262c5c521fe964f154ace6
Gil Burns <gil@gilburns.com>
2026-02-01 22:05:35 -0600

    Update yubicoauthenticator.sh

--- fragments/labels/yubicoauthenticator.sh
+++ fragments/labels/yubicoauthenticator.sh
@@ -2,6 +2,6 @@ yubicoauthenticator)
     name="Yubico Authenticator"
     type="dmg"
     downloadURL="https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator-latest-mac.dmg"
-    appNewVersion=""
+    appNewVersion=$(curl -sI "${downloadURL}" | tr -d '\r' | sed -n 's/^location:.*authenticator-\([0-9.]*\)-mac\.dmg/\1/p')
     expectedTeamID="LQA3CS5MM7"
     ;;
