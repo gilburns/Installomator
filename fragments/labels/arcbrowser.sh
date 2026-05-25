c6be1b56bd51234afd1b75d9fc3f3b9c6064d1fd
Gil Burns <gil@gilburns.com>
2026-05-25 17:12:40 -0500

    Update arcbrowser.sh

--- fragments/labels/arcbrowser.sh
+++ fragments/labels/arcbrowser.sh
@@ -1,7 +1,7 @@
 arcbrowser)
-name="Arc"
-type="dmg"
-downloadURL="https://releases.arc.net/release/Arc-latest.dmg"
-appNewVersion="$(curl -fsIL https://releases.arc.net/release/Arc-latest.dmg | grep -i ^location | sed -E 's/.*-([0-9]+\.[0-9]+\.[0-9]+-[0-9]+).*/\1/')"
-expectedTeamID="S6N382Y83G"
+    name="Arc"
+    type="dmg"
+    downloadURL="https://releases.arc.net/release/Arc-latest.dmg"
+    appNewVersion=$(curl -fsIL "$downloadURL" | grep -i ^location | tail -1 | sed 's/.*Arc-\([0-9]*\.[0-9]*\.[0-9]*\)-.*/\1/')
+    expectedTeamID="S6N382Y83G"
     ;;
